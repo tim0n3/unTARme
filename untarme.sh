656a82d0b9c2e338e6cf1b599df7b556489b9586
Tim0n3 <>
2023-08-31 13:09:50 +0200

    Wrapping main method in its own function

--- untarme.sh
+++ untarme.sh
@@ -18,45 +18,50 @@ log_or_print() {
     fi
 }
 
-# Parse command line options
-while getopts ":v" opt; do
-  case $opt in
-    v)
-      VERBOSE=true
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
+main() {
+    # Parse command line options
+    while getopts ":v" opt; do
+      case $opt in
+        v)
+          VERBOSE=true
+          ;;
+        \?)
+          echo "Invalid option: -$OPTARG" >&2
+          exit 1
+          ;;
+      esac
+    done
+
+    # Shift the options so that $1 now refers to the directory argument
+    shift "$((OPTIND-1))"
+
+    # Define the directory (use "$1" or a default directory if not provided)
+    directory="${1:-/path/to/your/directory}"
+
+    # Check if the directory exists
+    if [ ! -d "$directory" ]; then
+      log_or_print "Directory does not exist: $directory"
       exit 1
-      ;;
-  esac
-done
+    fi
 
-# Shift the options so that $1 now refers to the directory argument
-shift "$((OPTIND-1))"
-
-# Define the directory (use "$1" or a default directory if not provided)
-directory="${1:-/path/to/your/directory}"
-
-# Check if the directory exists
-if [ ! -d "$directory" ]; then
-  log_or_print "Directory does not exist: $directory"
-  exit 1
-fi
-
-# Use find to locate .tar.gz files and process them one by one
-find "$directory" -type f -name "*.tar.gz" -print0 | while IFS= read -r -d '' file; do
-  # Log and print the file extraction
-  log_or_print "Extracting $file"
+    # Use find to locate .tar.gz files and process them one by one
+    find "$directory" -type f -name "*.tar.gz" -print0 | while IFS= read -r -d '' file; do
+      # Log and print the file extraction
+      log_or_print "Extracting $file"
   
-  # Extract the file using tar with the specified options
-  tar -xvf "$file" --strip-components 3 >> "$LOG_FILE" 2>&1
+      # Extract the file using tar with the specified options
+      tar -xvf "$file" --strip-components 3 >> "$LOG_FILE" 2>&1
   
-  # Check if the extraction was successful
-  if [ $? -eq 0 ]; then
-    log_or_print "Extraction of $file completed successfully"
-  else
-    log_or_print "Extraction of $file failed"
-  fi
-done
+      # Check if the extraction was successful
+      if [ $? -eq 0 ]; then
+        log_or_print "Extraction of $file completed successfully"
+      else
+        log_or_print "Extraction of $file failed"
+      fi
+    done
 
-log_or_print "Extraction completed."
+    log_or_print "Extraction completed."
+
+}
+
+main$@
