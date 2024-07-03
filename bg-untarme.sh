8b6ff75a924bc9c01e1caa24335aaf7d476b8aba
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2024-07-03 11:06:06 +0200

    feat: native bash parrallel extract
    - limit the number of concurrent jobs to 4 to prevent overloading the Raspberry Pi's memory.

---
+++ bg-untarme.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+LOG_FILE="/home/pi/untar.log"
+VERBOSE=false
+
+log() {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1" >> "$LOG_FILE"
+}
+
+log_or_print() {
+    message="$1"
+    log "$message"
+    if [ "$VERBOSE" = true ]; then
+        echo "$message"
+    fi
+}
+
+extract_file() {
+    file="$1"
+    log_or_print "Extracting $file"
+    tar -xvf "$file" --strip-components 3 >> "$LOG_FILE" 2>&1
+    if [ $? -eq 0 ]; then
+        log_or_print "Extraction of $file completed successfully"
+    else
+        log_or_print "Extraction of $file failed"
+    fi
+}
+
+main() {
+    while getopts ":v" opt; do
+      case $opt in
+        v)
+          VERBOSE=true
+          ;;
+        \?)
+          echo "Invalid option: -$OPTARG" >&2
+          exit 1
+          ;;
+      esac
+    done
+
+    shift "$((OPTIND-1))"
+
+    directory="${1:-/path/to/your/directory}"
+
+    if [ ! -d "$directory" ]; then
+      log_or_print "Directory does not exist: $directory"
+      exit 1
+    fi
+
+    find "$directory" -type f -name "*.tar.gz" -print0 | while IFS= read -r -d '' file; do
+        extract_file "$file" &
+        # Limit the number of background jobs
+        if [[ $(jobs -r -p | wc -l) -ge 4 ]]; then
+            wait -n
+        fi
+    done
+
+    wait
+
+    log_or_print "Extraction completed."
+}
+
+main "$@"
+
